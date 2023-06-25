eac582ed1df9081cb9714cb27e85071d398571b3
Hasib Hossain <boraborhasib@gmail.com>
2023-06-25 20:39:17 +0600

    Problem 3 solved

---
+++ Problem-3.js
@@ -0,0 +1,14 @@
+const uniqueArr = (arr = []) => { 
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (!newArr.includes(element)) {
+            newArr.push(element)
+        }
+    }
+    return newArr;
+}
+
+const numArr = [1 , 2, 3, 1, 2, 4, 5, 5, 2, 3]; 
+
+console.log(uniqueArr(numArr))
